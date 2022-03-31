01298b60186c83c0a405949ca3965fb016218f7e
Jack Herrington <jack.herrington@elembio.com>
2022-03-31 14:50:48 -0700

    Responsive fixes

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import Head from "next/head";
 import {
   AppShell,
   Navbar,
@@ -18,62 +19,68 @@ export default function Layout({ children }) {
   const [opened, setOpened] = useState(false);
 
   return (
-    <AppShell
-      padding="md"
-      navbarOffsetBreakpoint="sm"
-      fixed
-      navbar={
-        <Navbar
-          p="md"
-          hiddenBreakpoint="sm"
-          hidden={!opened}
-          width={{ sm: 200 }}
-        >
-          <Link href="/" passHref>
-            <Box sx={{ display: "flex" }}>
-              <Home />
-              <Title order={5} ml={10}>
-                Home
-              </Title>
-            </Box>
-          </Link>
-        </Navbar>
-      }
-      header={
-        <Header
-          height={60}
-          p="xs"
-          sx={(theme) => ({
-            backgroundColor: theme.colors.blue[9],
-            color: "white",
-          })}
-        >
-          <div
-            style={{ display: "flex", alignItems: "center", height: "100%" }}
+    <div>
+      <Head>
+        <title>Oregon Humane Adoptable Dogs</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <AppShell
+        padding="md"
+        navbarOffsetBreakpoint="sm"
+        fixed
+        navbar={
+          <Navbar
+            p="md"
+            hiddenBreakpoint="sm"
+            hidden={!opened}
+            width={{ sm: 200 }}
           >
-            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[3]}
-                mr="xl"
-              />
-            </MediaQuery>
-            <DogBowl />
-            <Text ml={10} size="md">
-              Oregon Humane Adoptable Dogs
-            </Text>
-          </div>
-        </Header>
-      }
-      styles={(theme) => ({
-        main: {
-          backgroundColor: theme.colors.gray[0],
-        },
-      })}
-    >
-      {children}
-    </AppShell>
+            <Link href="/" passHref>
+              <Box sx={{ display: "flex" }}>
+                <Home />
+                <Title order={5} ml={10}>
+                  Home
+                </Title>
+              </Box>
+            </Link>
+          </Navbar>
+        }
+        header={
+          <Header
+            height={60}
+            p="xs"
+            sx={(theme) => ({
+              backgroundColor: theme.colors.blue[9],
+              color: "white",
+            })}
+          >
+            <div
+              style={{ display: "flex", alignItems: "center", height: "100%" }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[3]}
+                  mr="xl"
+                />
+              </MediaQuery>
+              <DogBowl />
+              <Text ml={10} size="md">
+                Oregon Humane Adoptable Dogs
+              </Text>
+            </div>
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor: theme.colors.gray[0],
+          },
+        })}
+      >
+        {children}
+      </AppShell>
+    </div>
   );
 }
