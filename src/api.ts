7d16e8448eba99441aac80bc5349adcfeaa89b89
Jack Herrington <jack.herrington@elembio.com>
2022-03-31 14:46:06 -0700

    Updates

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,11 @@ import { QueryClient } from "react-query";
 
 import { getSdk } from "../src/generated/graphql";
 
-const gqlClient = new GraphQLClient("http://localhost:3000/api/graphql");
+const gqlClient = new GraphQLClient(
+  process.env.NODE_ENV === "production"
+    ? "https://nextjs-gql-dogs.vercel.app/api/graphql"
+    : "http://localhost:3000/api/graphql"
+);
 export const { getDogs, dogByName } = getSdk(gqlClient);
 export const queryClient = new QueryClient({
   defaultOptions: {
